44270df02c7d8d50f89d032e238c4be4a79bf2eb
Eka Putra <ekaputra250406@gmail.com>
2026-06-24 04:42:42 +0700

    fix tailwind css and styling

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 
 export default defineConfig({
+  plugins: [react()],
 
- plugins:[
-  react(),
-  tailwindcss()
- ]
-
+  // tambahkan ini kalau mau deploy ke GitHub Pages
+  base: "/dayat/",
 })
